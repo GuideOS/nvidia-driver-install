0967f748f3c050700f763e07416ab4ecf3af0779
TechGameGeek <161472768+TechGameGeek@users.noreply.github.com>
2025-02-21 09:17:34 +0100

    Update nv_debian_upgrade_su.sh

--- nv_debian_upgrade_su.sh
+++ nv_debian_upgrade_su.sh
@@ -5,44 +5,44 @@ echo "Führe Installation unter Verwendung von 'su -' als Rootuser aus!"
 echo "Sofern kein Rootuser angelegt ist, bitte Script 'nv_debian_upgrade_sudo.sh' verwenden!"
 # Repository um contrib und non-free erweitern
 echo "Füge Standardrepositories die Zweige 'contrib' und 'non-free' hinzu..."
-apt-add-repository contrib non-free -y >/dev/null 2>&1
+apt-add-repository contrib non-free -y
 
 # Backports aktivieren
 echo "Aktiviere Bookworm-Backports..."
 REPO="deb http://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware"
 SOURCE_FILE="/etc/apt/sources.list"
 
-if grep -qF "$REPO" "$SOURCE_FILE" 2>/dev/null || grep -qF "$REPO" /etc/apt/sources.list.d/*.list 2>/dev/null; then
-    echo "Das Repository ist bereits eingetragen. Überspringe diesen Schritt..." >/dev/null
+if grep -qF "$REPO" "$SOURCE_FILE" || grep -qF "$REPO" /etc/apt/sources.list.d/*.list; then
+    echo "Das Repository ist bereits eingetragen. Überspringe diesen Schritt..."
 else
-    echo "Das Repository ist nicht eingetragen. Es wird hinzugefügt..." >/dev/null
-    echo "$REPO" | tee -a "$SOURCE_FILE" >/dev/null 2>&1
+    echo "Das Repository ist nicht eingetragen. Es wird hinzugefügt..."
+    echo "$REPO" | tee -a "$SOURCE_FILE"
 fi
 
 # System aktualisieren und Kernel installieren
 echo "Aktualisiere Paketquellen..."
-apt update >/dev/null 2>&1
+apt update
 echo "Installiere aktuelles Backportskernel & Header, DKMS und Firmware-nonfree..."
-apt install -y linux-image-6.12.9+bpo-amd64 linux-headers-6.12.9+bpo-amd64 dkms firmware-misc-nonfree firmware-linux-nonfree >/dev/null 2>&1
+apt install -y linux-image-6.12.9+bpo-amd64 linux-headers-6.12.9+bpo-amd64 dkms firmware-misc-nonfree firmware-linux-nonfree
 
 clear
 echo "Hole NVIDIA Keyring von NVIDIA-Server..."
 # Nvidia Keyring holen
-wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb >/dev/null 2>&1
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
 sleep 2
 
 # Keyring installieren
 echo "Installiere NVIDIA Keyring..."
-dpkg -i cuda-keyring_1.1-1_all.deb 2>&1
+dpkg -i cuda-keyring_1.1-1_all.deb
 sleep 2
-apt update >/dev/null 2>&1
-apt -f install >/dev/null 2>&1
+apt update
+apt -f install
 clear
 
 # NVIDIA Repository eintragen
 echo "Trage NVIDIA-CUDA Repository in /etc/apt/sources.list.d/ ein"
 echo "deb [signed-by=/usr/share/keyrings/cuda-archive-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/ /" \
-    | tee /etc/apt/sources.list.d/cuda-debian12-x86_64.list >/dev/null 2>&1
+    | tee /etc/apt/sources.list.d/cuda-debian12-x86_64.list
 sleep 2
 
 # Driverauswahl
@@ -58,16 +58,16 @@ read -p "Bitte wähle eine Option (1/2/3): " choice
 case $choice in
     1)
         echo "Installiere den offenen Nvidia-Treiber..."
-        apt install -y nvidia-open >/dev/null 2>&1
+        apt install -y nvidia-open
         echo "Bitte System neu starten..."
         ;;
     2)
         echo "Installiere den proprietären Nvidia-Treiber mit CUDA..."
-        apt install -y cuda-driver >/dev/null 2>&1
+        apt install -y cuda-driver
         echo "Bitte System neu starten..."
         ;;
     3)
-        echo "Installation abgebrochen." 
+        echo "Installation abgebrochen."
         exit 0
         ;;
     *)
